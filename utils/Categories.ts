bb5597d05baf5713aa4e982d81a83d7961c8c28b
ottojoash <louisjoshbricks@gmail.com>
2024-07-16 13:33:44 +0300

    first commit

---
+++ utils/Categories.ts
@@ -0,0 +1,35 @@
+import { AiFillPhone, AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+import { MdOutlineKeyboard, MdStorefront, MdTv, MdWatch,MdOutlineGavel } from "react-icons/md";
+import { FaTshirt } from 'react-icons/fa'; // Importing FaTshirt
+
+
+export const categories = [
+  {
+    label: "All",
+    icon: MdStorefront,
+  },
+  {
+    label: "Kaftan",
+    icon: FaTshirt,
+  },
+  {
+    label: "Suits",
+    icon: FaTshirt,
+  },
+  {
+    label: "Shirts",
+    icon: FaTshirt,
+  },
+  {
+    label: "Trousers",
+    icon: FaTshirt,
+  },
+  {
+    label: "Bags",
+    icon: FaTshirt,
+  },
+  {
+    label: "Accesories",
+    icon: FaTshirt,
+  },
+];
